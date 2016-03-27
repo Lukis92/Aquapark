066ed7ea1065633d43bd0412824927d7988858db
Łukasz Korol <lukasz.korol@unitedideas.pl>
2016-03-27 22:41:48 +0200

    Add mdb bootstrap files and create price panels with accordions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
     @contact = Contact.new
     @entry_types = EntryType.all
+    @tickets = EntryType.where(:kind => "Bilet")
+    @passes = EntryType.where(:kind => "Karnet")
+    @chipest_ticket = EntryType.order(:price).first
   end
 end
